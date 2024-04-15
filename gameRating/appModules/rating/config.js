8585197dfa8cf1a98fd0635282310651b354dfd4
Shamhadov <adwedelf@gmail.com>
2024-04-15 21:48:01 +0700

    workkkk

---
+++ gameRating/appModules/rating/config.js
@@ -0,0 +1,12 @@
+const PATH_TO_RATING = "./dataset/rating.json";
+
+const WEIGHT = {
+  gameplay: 2,
+  design: 1,
+  idea: 3
+}
+
+module.exports = {
+  PATH_TO_RATING,
+  WEIGHT
+};
---
+++ gameRating/controllers/index.js
@@ -0,0 +1,11 @@
+const defaultRouteController = require('./default');
+const mainRouteController = require('./main');
+const gameRouteController = require('./game');
+const voteRoureController = require('./vote');
+
+module.exports = {
+  defaultRouteController,
+  mainRouteController,
+  gameRouteController,
+  voteRoureController
+}
